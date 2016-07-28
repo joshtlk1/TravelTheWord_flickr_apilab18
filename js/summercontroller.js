ac446d8056bab19194f7088b3f6095c9da53704a
Travis Stumbo <tstumbo1@gmail.com>
2016-07-28 16:50:52 -0400

    summer ctrl

--- js/summercontroller.js
+++ js/summercontroller.js
@@ -1,5 +1,12 @@
 var app = angular.module("mySeasons");
 
-app.controller('$scope', ['summerCtrl', function($scope){
-	
-}]);
+app.controller('summerCtrl', function($scope, $http){
+	$http({
+  method: 'GET',
+  url: 'http://history.openweathermap.org/data/2.5/history/city?id={4990729}&type=hour&start={1466467200}&end={1469664000}'
+}).then(function successCallback(response) {
+   $scope.season = response.data.season;
+  }, function errorCallback(response) {
+    console.log(response);
+  });
+});
