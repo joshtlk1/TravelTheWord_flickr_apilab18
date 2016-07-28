dd3a85c8b5bd7ad601363f29cb9de5665fd14e61
Karen Langner <krenabeth@yahoo.com>
2016-07-28 16:48:43 -0400

    adding in the palindrome and 404 page

--- js/wintercontroller.js
+++ js/wintercontroller.js
@@ -1,5 +1,29 @@
 var app = angular.module("mySeasons");
 
 app.controller('$scope', ['winterCtrl', function($scope){
+	$scope.checkForPalindrome = function(palindrome) {
+	// palindrome is our string
+  	// new empty string to push to
+  		var fantasticString = "";
+  		for (var i = palindrome.length - 1; i >= 0; i--) { 
+  	// make it lowercase
+    		palindrome.toLowerCase();
+    // as long as it's not a space, add the letter to the string
+    		if (palindrome[i]) {
+     			fantasticString += palindrome[i];
+    		} 
+    		console.log(fantasticString);
+    	}
+  	// does james === semaj?
+      	if(fantasticString === palindrome) {
+		// if yes, return "Great that's a palindrome!"
+        console.log("A Santa at Nasa! That's a palindrome!");
+      	} else {
+		// if no, then "Try again!"
+      	console.log("A dog! A panic in a pagoda! Try again!");
+      	}
+  	return fantasticString;
+
+	};// close out checkForPalindrome
 	
 }]);
