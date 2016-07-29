5ba73053e3c4eb8db749447be41cadb9e0ed47b2
Josh Teo <joshua.teoleekuok@gmail.com>
2016-07-28 21:43:04 -0400

    first commit

--- wintercontroller.js
+++ wintercontroller.js
@@ -2,6 +2,10 @@ var app = angular.module("mySeasons");
 
 app.controller('$scope', ['winterCtrl', function($scope){
 	$scope.checkForPalindrome = function(palindrome) {
-		
+		//string value ex.james
+		// split string into array
+		//forEach alphabet push it into an empty array
+		//empty array will now be a new variable ex.semaj
+		//if james === semaj return, "Yes, it is a Palindrome!"
 	}
 }]);
