2b32d9db1ae0737a39f901649196980bd5463971
Erika Bergman <erika.a.bergman@gmail.com>
2016-07-29 13:32:05 -0700

    summerCtrl

--- js/summercontroller.js
+++ js/summercontroller.js
@@ -3,9 +3,9 @@ var app = angular.module("mySeasons");
 app.controller('summerCtrl', function($scope, $http){
 	$http({
   method: 'GET',
-  url: 'https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=JSON_CALLBACK'
+  url:"https://teamtreehouse.com/erikabergman.json"
 }).then(function successCallback(response) {
-   $scope.season = response.data.season;
+   $scope.badges = response.badges;
   }, function errorCallback(response) {
     console.log(response);
   });
