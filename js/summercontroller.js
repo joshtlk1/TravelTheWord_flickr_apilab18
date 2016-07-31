e01ff2b9f38b58b8f3365265d59ed7f7ba8bed3a
Josh Teo <joshua.teoleekuok@gmail.com>
2016-07-30 22:05:31 -0400

    api working

--- js/summercontroller.js
+++ js/summercontroller.js
@@ -1,12 +1,38 @@
 var app = angular.module("mySeasons");
 
 app.controller('summerCtrl', function($scope, $http){
-	$http({
-  method: 'GET',
-  url:"https://teamtreehouse.com/erikabergman.json"
-}).then(function successCallback(response) {
-   $scope.badges = response.badges;
-  }, function errorCallback(response) {
-    console.log(response);
-  });
+	$scope.list;
+
+	 $http.jsonp('https://api.flickr.com/services/feeds/photos_public.gne?format=json').success(function (data) {
+      
+    });
+	
+	jsonFlickrFeed = function(data){
+	$scope.photos = data.items;
+	 console.log($scope.photos);
+	}
+  
+
+
+
+
+
+
+
+
+
+
+
+
+// 	$http({
+//   method: 'GET',
+//   url:"http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?"
+// }).then(function successCallback(response) {
+//    $scope.items = response.data.items;
+//   }, function errorCallback(response) {
+//     console.log(response);
+//   });
 });
+
+
+
